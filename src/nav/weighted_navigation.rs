eb9140ff1b10876aebe5171eb71e91aa83dcfa3d
drwadu <dominik.rusovac@hotmail.com>
2023-03-23 21:06:27 +0100

    update.

--- src/nav/weighted_navigation.rs
+++ src/nav/weighted_navigation.rs
@@ -1,55 +1,54 @@
 use super::faceted_navigation::{fs_stats, FacetedNavigation};
 
 /// TODO
-pub fn count<S: ToString>(
-    nav: &mut impl WeightedNavigation,
+pub fn count<S: ToString, T>(
+    sharp: &mut impl WeightedNavigation<T>,
+    nav: &mut T,
     peek_on: (impl Iterator<Item = S>, impl Iterator<Item = S>),
 ) -> Option<(usize, Option<usize>)> {
-    nav.eval_sharp(peek_on)
+    sharp.eval_sharp(nav, peek_on)
 }
 
-pub trait WeightedNavigation {
+pub trait WeightedNavigation<T> {
     fn eval_sharp<S: ToString>(
         &mut self,
+        nav: &mut T,
         peek_on: (impl Iterator<Item = S>, impl Iterator<Item = S>),
     ) -> Option<(usize, Option<usize>)>;
     fn eval_sharp_restricted<S: ToString>(
         &mut self,
+        nav: &mut T,
         peek_on: (impl Iterator<Item = S>, impl Iterator<Item = S>),
         target: &[S],
     ) -> Option<(usize, Option<usize>)>;
 }
 
 #[derive(Debug, Clone)]
-pub enum Sharp<T> {
-    AnswerSetCounting(T),
-    FacetCounting(T),
-    BcCounting(T),
-    CcCounting(T),
+pub enum Sharp {
+    AnswerSetCounting,
+    FacetCounting,
+    BcCounting,
+    CcCounting,
 }
 
-impl<T: FacetedNavigation> WeightedNavigation for Sharp<T> {
+impl<T: FacetedNavigation> WeightedNavigation<T> for Sharp {
     fn eval_sharp<S: ToString>(
         &mut self,
+        nav: &mut T,
         peek_on: (impl Iterator<Item = S>, impl Iterator<Item = S>),
     ) -> Option<(usize, Option<usize>)> {
         match self {
-            Self::FacetCounting(nav) => {
-                fs_stats(nav, peek_on).and_then(|(_, _, fsc)| Some((fsc, None)))
-            }
-            Self::AnswerSetCounting(_) => {
+            Self::FacetCounting => fs_stats(nav, peek_on).and_then(|(_, _, fsc)| Some((fsc, None))),
+            Self::AnswerSetCounting => {
                 todo!()
             }
-            Self::BcCounting(nav) => {
-                fs_stats(nav, peek_on).and_then(|(bcc, _, _)| Some((bcc, None)))
-            }
-            Self::CcCounting(nav) => {
-                fs_stats(nav, peek_on).and_then(|(_, ccc, _)| Some((ccc, None)))
-            }
+            Self::BcCounting => fs_stats(nav, peek_on).and_then(|(bcc, _, _)| Some((bcc, None))),
+            Self::CcCounting => fs_stats(nav, peek_on).and_then(|(_, ccc, _)| Some((ccc, None))),
         }
     }
     fn eval_sharp_restricted<S: ToString>(
         &mut self,
+        _nav: &mut T,
         _peek_on: (impl Iterator<Item = S>, impl Iterator<Item = S>),
         _target: &[S],
     ) -> Option<(usize, Option<usize>)> {
