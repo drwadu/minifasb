8363e109d1ee82999cabd5c40a315db71de8ca08
drwadu <dominik.rusovac@hotmail.com>
2023-04-29 12:34:15 +0200

    update.

--- src/nav/faceted_navigation.rs
+++ src/nav/faceted_navigation.rs
@@ -8,7 +8,7 @@ use super::Navigator;
 /// TODO
 pub fn bc(
     nav: &mut impl FacetedNavigation,
-    peek_on: (impl Iterator<Item = String>, impl Iterator<Item = String>),
+    peek_on: impl Iterator<Item = String>,
 ) -> Option<Vec<Symbol>> {
     nav.brave_consequences(peek_on)
 }
@@ -16,7 +16,7 @@ pub fn bc(
 /// TODO
 pub fn cc(
     nav: &mut impl FacetedNavigation,
-    peek_on: (impl Iterator<Item = String>, impl Iterator<Item = String>),
+    peek_on: impl Iterator<Item = String>,
 ) -> Option<Vec<Symbol>> {
     nav.cautious_consequences(peek_on)
 }
@@ -24,7 +24,7 @@ pub fn cc(
 /// TODO
 pub fn fs<S: ToString>(
     nav: &mut impl FacetedNavigation,
-    peek_on: (impl Iterator<Item = S>, impl Iterator<Item = S>),
+    peek_on: impl Iterator<Item = S>,
 ) -> Option<HashSet<Symbol>> {
     nav.facets(peek_on)
 }
@@ -32,227 +32,88 @@ pub fn fs<S: ToString>(
 /// TODO
 pub fn fs_stats<S: ToString>(
     nav: &mut impl FacetedNavigation,
-    peek_on: (impl Iterator<Item = S>, impl Iterator<Item = S>),
+    peek_on: impl Iterator<Item = S>,
 ) -> Option<(usize, usize, usize)> {
     nav.stats(peek_on)
+}
+
+fn nav_route<S: ToString>(
+    state: &mut Navigation,
+    peek_on: impl Iterator<Item = S>,
+) -> (&mut Navigator, Vec<SolverLiteral>) {
+    match state {
+        Navigation::And(nav) | Navigation::AndOr(nav) => {
+            let mut route = peek_on
+                .map(|f| {
+                    let s = f.to_string();
+                    match s.starts_with("~") {
+                        true => parse(&s[1..])
+                            .map(|symbol| nav.literals.get(&symbol).map(|l| l.negate()))
+                            .flatten(),
+                        _ => parse(&s)
+                            .map(|symbol| nav.literals.get(&symbol))
+                            .flatten()
+                            .copied(),
+                    }
+                })
+                .flatten()
+                .collect::<Vec<_>>();
+            route.extend(nav.conjuncts.0.clone());
+
+            (nav, route)
+        }
+    }
 }
 
 pub trait FacetedNavigation {
     fn brave_consequences<S: ToString>(
         &mut self,
-        peek_on: (impl Iterator<Item = S>, impl Iterator<Item = S>),
+        peek_on: impl Iterator<Item = S>,
     ) -> Option<Vec<Symbol>>;
     fn cautious_consequences<S: ToString>(
         &mut self,
-        peek_on: (impl Iterator<Item = S>, impl Iterator<Item = S>),
+        peek_on: impl Iterator<Item = S>,
     ) -> Option<Vec<Symbol>>;
-    fn facets<S: ToString>(
-        &mut self,
-        peek_on: (impl Iterator<Item = S>, impl Iterator<Item = S>),
-    ) -> Option<HashSet<Symbol>>;
+    fn facets<S: ToString>(&mut self, peek_on: impl Iterator<Item = S>) -> Option<HashSet<Symbol>>;
     fn stats<S: ToString>(
         &mut self,
-        peek_on: (impl Iterator<Item = S>, impl Iterator<Item = S>),
+        peek_on: impl Iterator<Item = S>,
     ) -> Option<(usize, usize, usize)>;
 }
 impl FacetedNavigation for Navigation {
     fn brave_consequences<S: ToString>(
         &mut self,
-        peek_on: (impl Iterator<Item = S>, impl Iterator<Item = S>),
+        peek_on: impl Iterator<Item = S>,
     ) -> Option<Vec<Symbol>> {
-        let (nav, route) = match self {
-            Self::And(nav) => {
-                let mut route = peek_on
-                    .0
-                    .map(|f| {
-                        let s = f.to_string();
-                        match s.starts_with("~") {
-                            true => parse(&s[1..])
-                                .map(|symbol| nav.literals.get(&symbol).map(|l| l.negate()))
-                                .flatten(),
-                            _ => parse(&s)
-                                .map(|symbol| nav.literals.get(&symbol))
-                                .flatten()
-                                .copied(),
-                        }
-                    })
-                    .flatten()
-                    .collect::<Vec<_>>();
-                route.extend(nav.route.1.clone());
+        let (mut nav, route) = nav_route(self, peek_on);
 
-                (nav, route)
-            }
-            Self::AndOr(nav) => {
-                let route = peek_on
-                    .1
-                    .map(|f| {
-                        let s = f.to_string();
-                        match s.starts_with("~") {
-                            true => parse(&s[1..]),
-                            _ => parse(&s),
-                        }
-                    })
-                    .flatten()
-                    .collect::<Vec<_>>();
-                if !route.is_empty() {
-                    nav.or_delta(route.iter());
-                }
-                let mut route = peek_on
-                    .0
-                    .map(|f| {
-                        let s = f.to_string();
-                        match s.starts_with("~") {
-                            true => parse(&s[1..])
-                                .map(|symbol| nav.literals.get(&symbol).map(|l| l.negate()))
-                                .flatten(),
-                            _ => parse(&s)
-                                .map(|symbol| nav.literals.get(&symbol))
-                                .flatten()
-                                .copied(),
-                        }
-                    })
-                    .flatten()
-                    .collect::<Vec<_>>();
-                route.extend(nav.route.1.clone());
-
-                (nav, route)
-            }
-        };
-
-        consequences(Consequences::Brave, nav, &route)
+        consequences(Consequences::Brave, &mut nav, &route)
     }
 
     fn cautious_consequences<S: ToString>(
         &mut self,
-        peek_on: (impl Iterator<Item = S>, impl Iterator<Item = S>),
+        peek_on: impl Iterator<Item = S>,
     ) -> Option<Vec<Symbol>> {
-        let (nav, route) = match self {
-            Self::And(nav) => {
-                let mut route = peek_on
-                    .0
-                    .map(|f| {
-                        let s = f.to_string();
-                        match s.starts_with("~") {
-                            true => parse(&s[1..])
-                                .map(|symbol| nav.literals.get(&symbol).map(|l| l.negate()))
-                                .flatten(),
-                            _ => parse(&s)
-                                .map(|symbol| nav.literals.get(&symbol))
-                                .flatten()
-                                .copied(),
-                        }
-                    })
-                    .flatten()
-                    .collect::<Vec<_>>();
-                route.extend(nav.route.1.clone());
+        //let (nav, route) = match self {
+        //    Self::And(nav) | Self::AndOr(nav) => {
+        //        let mut route = read_peek_on(peek_on, &nav);
+        //        route.extend(nav.conjuncts.0.clone());
 
-                (nav, route)
-            }
-            Self::AndOr(nav) => {
-                let route = peek_on
-                    .1
-                    .map(|f| {
-                        let s = f.to_string();
-                        match s.starts_with("~") {
-                            true => parse(&s[1..]),
-                            _ => parse(&s),
-                        }
-                    })
-                    .flatten()
-                    .collect::<Vec<_>>();
-                if !route.is_empty() {
-                    nav.or_delta(route.iter());
-                }
-                let mut route = peek_on
-                    .0
-                    .map(|f| {
-                        let s = f.to_string();
-                        match s.starts_with("~") {
-                            true => parse(&s[1..])
-                                .map(|symbol| nav.literals.get(&symbol).map(|l| l.negate()))
-                                .flatten(),
-                            _ => parse(&s)
-                                .map(|symbol| nav.literals.get(&symbol))
-                                .flatten()
-                                .copied(),
-                        }
-                    })
-                    .flatten()
-                    .collect::<Vec<_>>();
-                route.extend(nav.route.1.clone());
+        //        (nav, route)
+        //    }
+        //};
+        let (mut nav, route) = nav_route(self, peek_on);
 
-                (nav, route)
-            }
-        };
-
-        consequences(Consequences::Cautious, nav, &route)
+        consequences(Consequences::Cautious, &mut nav, &route)
     }
 
-    fn facets<S: ToString>(
-        &mut self,
-        peek_on: (impl Iterator<Item = S>, impl Iterator<Item = S>),
-    ) -> Option<HashSet<Symbol>> {
-        let (nav, route) = match self {
-            Self::And(nav) => {
-                let mut route = peek_on
-                    .0
-                    .map(|f| {
-                        let s = f.to_string();
-                        match s.starts_with("~") {
-                            true => parse(&s[1..])
-                                .map(|symbol| nav.literals.get(&symbol).map(|l| l.negate()))
-                                .flatten(),
-                            _ => parse(&s)
-                                .map(|symbol| nav.literals.get(&symbol))
-                                .flatten()
-                                .copied(),
-                        }
-                    })
-                    .flatten()
-                    .collect::<Vec<_>>();
-                route.extend(nav.route.1.clone());
-                (nav, route)
-            }
-            Self::AndOr(nav) => {
-                let route = peek_on
-                    .1
-                    .map(|f| {
-                        let s = f.to_string();
-                        match s.starts_with("~") {
-                            true => parse(&s[1..]),
-                            _ => parse(&s),
-                        }
-                    })
-                    .flatten()
-                    .collect::<Vec<_>>();
-                if !route.is_empty() {
-                    nav.or_delta(route.iter());
-                }
-                let mut route = peek_on
-                    .0
-                    .map(|f| {
-                        let s = f.to_string();
-                        match s.starts_with("~") {
-                            true => parse(&s[1..])
-                                .map(|symbol| nav.literals.get(&symbol).map(|l| l.negate()))
-                                .flatten(),
-                            _ => parse(&s)
-                                .map(|symbol| nav.literals.get(&symbol))
-                                .flatten()
-                                .copied(),
-                        }
-                    })
-                    .flatten()
-                    .collect::<Vec<_>>();
-                route.extend(nav.route.1.clone());
+    fn facets<S: ToString>(&mut self, peek_on: impl Iterator<Item = S>) -> Option<HashSet<Symbol>> {
+        let (mut nav, route) = nav_route(self, peek_on);
 
-                (nav, route)
-            }
-        };
+        let bcs = consequences(Consequences::Brave, &mut nav, &route)?;
 
-        let bcs = consequences(Consequences::Brave, nav, &route)?;
         match !bcs.is_empty() {
-            true => consequences(Consequences::Cautious, nav, &route)
+            true => consequences(Consequences::Cautious, &mut nav, &route)
                 .as_ref()
                 .and_then(|ccs| Some(bcs.difference_as_set(ccs))),
             _ => Some(bcs.to_hashset()),
@@ -261,70 +122,14 @@ impl FacetedNavigation for Navigation {
 
     fn stats<S: ToString>(
         &mut self,
-        peek_on: (impl Iterator<Item = S>, impl Iterator<Item = S>),
+        peek_on: impl Iterator<Item = S>,
     ) -> Option<(usize, usize, usize)> {
-        let (nav, route) = match self {
-            Self::And(nav) => {
-                let mut route = peek_on
-                    .0
-                    .map(|f| {
-                        let s = f.to_string();
-                        match s.starts_with("~") {
-                            true => parse(&s[1..])
-                                .map(|symbol| nav.literals.get(&symbol).map(|l| l.negate()))
-                                .flatten(),
-                            _ => parse(&s)
-                                .map(|symbol| nav.literals.get(&symbol))
-                                .flatten()
-                                .copied(),
-                        }
-                    })
-                    .flatten()
-                    .collect::<Vec<_>>();
-                route.extend(nav.route.1.clone());
-                (nav, route)
-            }
-            Self::AndOr(nav) => {
-                let route = peek_on
-                    .1
-                    .map(|f| {
-                        let s = f.to_string();
-                        match s.starts_with("~") {
-                            true => parse(&s[1..]),
-                            _ => parse(&s),
-                        }
-                    })
-                    .flatten()
-                    .collect::<Vec<_>>();
-                if !route.is_empty() {
-                    nav.or_delta(route.iter());
-                }
-                let mut route = peek_on
-                    .0
-                    .map(|f| {
-                        let s = f.to_string();
-                        match s.starts_with("~") {
-                            true => parse(&s[1..])
-                                .map(|symbol| nav.literals.get(&symbol).map(|l| l.negate()))
-                                .flatten(),
-                            _ => parse(&s)
-                                .map(|symbol| nav.literals.get(&symbol))
-                                .flatten()
-                                .copied(),
-                        }
-                    })
-                    .flatten()
-                    .collect::<Vec<_>>();
-                route.extend(nav.route.1.clone());
+        let (mut nav, route) = nav_route(self, peek_on);
 
-                (nav, route)
-            }
-        };
-
-        let bcs = consequences(Consequences::Brave, nav, &route)?;
+        let bcs = consequences(Consequences::Brave, &mut nav, &route)?;
         match !bcs.is_empty() {
             true => {
-                let ccs = consequences(Consequences::Cautious, nav, &route)?;
+                let ccs = consequences(Consequences::Cautious, &mut nav, &route)?;
                 Some((
                     bcs.len(),
                     ccs.len(),
--- src/nav/weighted_navigation.rs
+++ src/nav/weighted_navigation.rs
@@ -4,7 +4,7 @@ use super::faceted_navigation::{fs_stats, FacetedNavigation};
 pub fn count<S: ToString, T>(
     sharp: &mut impl WeightedNavigation<T>,
     nav: &mut T,
-    peek_on: (impl Iterator<Item = S>, impl Iterator<Item = S>),
+    peek_on: impl Iterator<Item = S>,
 ) -> Option<(usize, Option<usize>)> {
     sharp.eval_sharp(nav, peek_on)
 }
@@ -13,12 +13,12 @@ pub trait WeightedNavigation<T> {
     fn eval_sharp<S: ToString>(
         &mut self,
         nav: &mut T,
-        peek_on: (impl Iterator<Item = S>, impl Iterator<Item = S>),
+        peek_on: impl Iterator<Item = S>,
     ) -> Option<(usize, Option<usize>)>;
     fn eval_sharp_restricted<S: ToString>(
         &mut self,
         nav: &mut T,
-        peek_on: (impl Iterator<Item = S>, impl Iterator<Item = S>),
+        peek_on: impl Iterator<Item = S>,
         target: &[S],
     ) -> Option<(usize, Option<usize>)>;
 }
@@ -35,7 +35,7 @@ impl<T: FacetedNavigation> WeightedNavigation<T> for Sharp {
     fn eval_sharp<S: ToString>(
         &mut self,
         nav: &mut T,
-        peek_on: (impl Iterator<Item = S>, impl Iterator<Item = S>),
+        peek_on: impl Iterator<Item = S>,
     ) -> Option<(usize, Option<usize>)> {
         match self {
             Self::FacetCounting => fs_stats(nav, peek_on).and_then(|(_, _, fsc)| Some((fsc, None))),
@@ -49,7 +49,7 @@ impl<T: FacetedNavigation> WeightedNavigation<T> for Sharp {
     fn eval_sharp_restricted<S: ToString>(
         &mut self,
         _nav: &mut T,
-        _peek_on: (impl Iterator<Item = S>, impl Iterator<Item = S>),
+        _peek_on: impl Iterator<Item = S>,
         _target: &[S],
     ) -> Option<(usize, Option<usize>)> {
         todo!()
