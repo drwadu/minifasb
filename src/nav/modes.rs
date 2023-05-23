b161b470b45e5bbdb132e8201235124b7da0b34f
drwadu <dominik.rusovac@hotmail.com>
2023-05-23 16:25:15 +0200

    update.

--- src/nav/modes.rs
+++ src/nav/modes.rs
@@ -98,7 +98,7 @@ impl Guide for Mode {
                         f = Some((format!("~{sym}"), ln));
                     }
                     active.pop();
-
+                    dbg!(&f,curr);
                 }
 
                 f
