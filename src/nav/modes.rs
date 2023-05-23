c04f8f5787bd0f1ce8351098d821a7c707953f13
drwadu <dominik.rusovac@hotmail.com>
2023-05-23 14:49:09 +0200

    update.

--- src/nav/modes.rs
+++ src/nav/modes.rs
@@ -14,15 +14,13 @@ pub trait Guide {
     fn step(&mut self, nav: &mut Navigator) -> Option<(String, SolverLiteral)>;
 }
 impl Guide for Mode {
+    // TODO: only for AND
     fn step(&mut self, nav: &mut Navigator) -> Option<(String, SolverLiteral)> {
-        nav.ctl
-            .backend()
-            .and_then(|mut b| b.assume(&nav.conjuncts.0))
-            .ok()?;
-        let bc = consequences(Consequences::Brave, nav, &[])?;
+        let mut active = nav.conjuncts.0.clone();
+        let bc = consequences(Consequences::Brave, nav, &active)?;
         let fs = match !bc.is_empty() {
             true => unsafe {
-                consequences(Consequences::Cautious, nav, &[])
+                consequences(Consequences::Cautious, nav, &active)
                     .as_ref()
                     .map(|ccs| {
                         bc.difference_as_set(&ccs)
@@ -41,12 +39,13 @@ impl Guide for Mode {
                 .into_iter()
                 .next()
                 .and_then(|f| Some((f.to_string(), *unsafe { lits.get(&f).unwrap_unchecked() }))),
-            Self::MinWeighted(Weight::FacetCounting) => {
+            Self::MaxWeighted(Weight::FacetCounting) => {
                 let (mut min, mut f): (usize, Option<(String, SolverLiteral)>) = (1, None);
                 for sym in fs {
                     let l = unsafe { lits.get(&sym).unwrap_unchecked() };
-                    let bc = consequences(Consequences::Brave, nav, &[*l])?;
-                    let cc = consequences(Consequences::Cautious, nav, &[*l])?;
+                    active.push(*l);
+                    let bc = consequences(Consequences::Brave, nav, &active)?;
+                    let cc = consequences(Consequences::Cautious, nav, &active)?;
                     let count = bc.to_hashset().difference(&cc.to_hashset()).count();
                     if count == 1 {
                         return Some((sym.to_string(), *l));
@@ -55,10 +54,12 @@ impl Guide for Mode {
                         min = count;
                         f = Some((sym.to_string(), *l));
                     }
+                    active.pop();
 
                     let ln = l.negate();
-                    let bc = consequences(Consequences::Brave, nav, &[ln])?;
-                    let cc = consequences(Consequences::Cautious, nav, &[ln])?;
+                    active.push(ln);
+                    let bc = consequences(Consequences::Brave, nav, &active)?;
+                    let cc = consequences(Consequences::Cautious, nav, &active)?;
                     let count = bc.to_hashset().difference(&cc.to_hashset()).count();
                     if count == 1 {
                         return Some((format!("~{sym}"), ln));
@@ -67,131 +68,153 @@ impl Guide for Mode {
                         min = count;
                         f = Some((format!("~{sym}"), ln));
                     }
-                }
-
-                f
-            }
-            Self::MinWeighted(Weight::AnswerSetCounting) => {
-                let (mut min, mut f): (usize, Option<(String, SolverLiteral)>) = (0, None);
-                for sym in fs {
-                    let l = unsafe { lits.get(&sym).unwrap_unchecked() };
-                    let count = answer_set_count(nav, &[*l], min).ok()?;
-                    if count == 1 {
-                        return Some((sym.to_string(), *l));
-                    }
-                    if count <= min {
-                        min = count;
-                        f = Some((sym.to_string(), *l));
-                    }
-
-                    let ln = l.negate();
-                    let count = answer_set_count(nav, &[ln], min).ok()?;
-                    if count == 1 {
-                        return Some((format!("~{sym}"), ln));
-                    }
-                    if count <= min {
-                        min = count;
-                        f = Some((format!("~{sym}"), ln));
-                    }
-                }
-
-                f
-            }
-            Self::MinWeighted(Weight::SupportedModelCounting) => {
-                eprintln!("ensure --supp-models flag was specified at startup.");
-                let (mut min, mut f): (usize, Option<(String, SolverLiteral)>) = (0, None);
-                for sym in fs {
-                    let l = unsafe { lits.get(&sym).unwrap_unchecked() };
-                    let count = answer_set_count(nav, &[*l], min).ok()?;
-                    if count == 1 {
-                        return Some((sym.to_string(), *l));
-                    }
-                    if count <= min {
-                        min = count;
-                        f = Some((sym.to_string(), *l));
-                    }
-
-                    let ln = l.negate();
-                    let count = answer_set_count(nav, &[ln], min).ok()?;
-                    if count == 1 {
-                        return Some((format!("~{sym}"), ln));
-                    }
-                    if count <= min {
-                        min = count;
-                        f = Some((format!("~{sym}"), ln));
-                    }
-                }
-
-                f
-            }
-            Self::MaxWeighted(Weight::FacetCounting) => {
-                let ub = fs.len();
-                let (mut max, mut f): (usize, Option<(String, SolverLiteral)>) = (ub, None);
-                for sym in fs {
-                    let l = unsafe { lits.get(&sym).unwrap_unchecked() };
-                    let bc = consequences(Consequences::Brave, nav, &[*l])?;
-                    let cc = consequences(Consequences::Cautious, nav, &[*l])?;
-                    let count = bc.to_hashset().difference(&cc.to_hashset()).count();
-                    if count == ub {
-                        return Some((sym.to_string(), *l));
-                    }
-                    if max <= count {
-                        max = count;
-                        f = Some((sym.to_string(), *l));
-                    }
-
-                    let ln = l.negate();
-                    let bc = consequences(Consequences::Brave, nav, &[ln])?;
-                    let cc = consequences(Consequences::Cautious, nav, &[ln])?;
-                    let count = bc.to_hashset().difference(&cc.to_hashset()).count();
-                    if count == ub {
-                        return Some((format!("~{sym}"), ln));
-                    }
-                    if max <= count {
-                        max = count;
-                        f = Some((format!("~{sym}"), ln));
-                    }
+                    active.pop();
                 }
 
                 f
             }
             Self::MaxWeighted(Weight::AnswerSetCounting) => {
-                let (mut max, mut f): (usize, Option<(String, SolverLiteral)>) = (0, None);
+                let (mut min, mut f): (usize, Option<(String, SolverLiteral)>) = (0, None);
                 for sym in fs {
                     let l = unsafe { lits.get(&sym).unwrap_unchecked() };
-                    let count = answer_set_count(nav, &[*l], max).ok()?;
-                    if max <= count {
-                        max = count;
+                    active.push(*l);
+                    let count = answer_set_count(nav, &active, min).ok()?;
+                    if count == 1 {
+                        return Some((sym.to_string(), *l));
+                    }
+                    if count <= min {
+                        min = count;
                         f = Some((sym.to_string(), *l));
                     }
+                    active.pop();
 
                     let ln = l.negate();
-                    let count = answer_set_count(nav, &[ln], max).ok()?;
-                    if max <= count {
-                        max = count;
+                    active.push(ln);
+                    let count = answer_set_count(nav, &active, min).ok()?;
+                    if count == 1 {
+                        return Some((format!("~{sym}"), ln));
+                    }
+                    if count <= min {
+                        min = count;
                         f = Some((format!("~{sym}"), ln));
                     }
+                    active.pop();
+
                 }
 
                 f
             }
             Self::MaxWeighted(Weight::SupportedModelCounting) => {
                 eprintln!("ensure --supp-models flag was specified at startup.");
+                let (mut min, mut f): (usize, Option<(String, SolverLiteral)>) = (0, None);
+                for sym in fs {
+                    let l = unsafe { lits.get(&sym).unwrap_unchecked() };
+                    active.push(*l);
+                    let count = answer_set_count(nav, &active, min).ok()?;
+                    if count == 1 {
+                        return Some((sym.to_string(), *l));
+                    }
+                    if count <= min {
+                        min = count;
+                        f = Some((sym.to_string(), *l));
+                    }
+                    active.pop();
+
+                    let ln = l.negate();
+                    active.push(ln);
+                    let count = answer_set_count(nav, &active, min).ok()?;
+                    if count == 1 {
+                        return Some((format!("~{sym}"), ln));
+                    }
+                    if count <= min {
+                        min = count;
+                        f = Some((format!("~{sym}"), ln));
+                    }
+                    active.pop();
+                }
+
+                f
+            }
+            Self::MinWeighted(Weight::FacetCounting) => {
+                let ub = fs.len();
+                let (mut max, mut f): (usize, Option<(String, SolverLiteral)>) = (ub, None);
+                for sym in fs {
+                    let l = unsafe { lits.get(&sym).unwrap_unchecked() };
+                    active.push(*l);
+                    let bc = consequences(Consequences::Brave, nav, &active)?;
+                    let cc = consequences(Consequences::Cautious, nav, &active)?;
+                    let count = bc.to_hashset().difference(&cc.to_hashset()).count();
+                    if count == ub {
+                        return Some((sym.to_string(), *l));
+                    }
+                    if max <= count {
+                        max = count;
+                        f = Some((sym.to_string(), *l));
+                    }
+                    active.pop();
+
+                    let ln = l.negate();
+                    active.push(ln);
+                    let bc = consequences(Consequences::Brave, nav, &active)?;
+                    let cc = consequences(Consequences::Cautious, nav, &active)?;
+                    let count = bc.to_hashset().difference(&cc.to_hashset()).count();
+                    if count == ub {
+                        return Some((format!("~{sym}"), ln));
+                    }
+                    if max <= count {
+                        max = count;
+                        f = Some((format!("~{sym}"), ln));
+                    }
+                    active.pop();
+                }
+
+                f
+            }
+            Self::MinWeighted(Weight::AnswerSetCounting) => {
                 let (mut max, mut f): (usize, Option<(String, SolverLiteral)>) = (0, None);
                 for sym in fs {
                     let l = unsafe { lits.get(&sym).unwrap_unchecked() };
+                    active.push(*l);
+                    let count = answer_set_count(nav, &active, max).ok()?;
+                    if max <= count {
+                        max = count;
+                        f = Some((sym.to_string(), *l));
+                    }
+                    active.pop();
+
+                    let ln = l.negate();
+                    active.push(ln);
+                    let count = answer_set_count(nav, &active, max).ok()?;
+                    if max <= count {
+                        max = count;
+                        f = Some((format!("~{sym}"), ln));
+                    }
+                    active.pop();
+                }
+
+                f
+            }
+            Self::MinWeighted(Weight::SupportedModelCounting) => {
+                eprintln!("ensure --supp-models flag was specified at startup.");
+                let (mut max, mut f): (usize, Option<(String, SolverLiteral)>) = (0, None);
+                for sym in fs {
+                    let l = unsafe { lits.get(&sym).unwrap_unchecked() };
+                    active.push(*l);
                     let count = answer_set_count(nav, &[*l], max).ok()?;
                     if max <= count {
                         max = count;
                         f = Some((sym.to_string(), *l));
                     }
+                    active.pop();
 
                     let ln = l.negate();
+                    active.push(ln);
                     let count = answer_set_count(nav, &[ln], max).ok()?;
                     if max <= count {
                         max = count;
                         f = Some((format!("~{sym}"), ln));
                     }
+                    active.pop();
                 }
 
                 f
