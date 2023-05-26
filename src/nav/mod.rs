251174db7f252253a440b3835314d5e7c820411d
drwadu <dominik.rusovac@hotmail.com>
2023-05-26 15:36:57 +0200

    update.

--- src/nav/mod.rs
+++ src/nav/mod.rs
@@ -131,8 +131,8 @@ impl Navigator {
                         .collect::<Vec<_>>() // TODO: mäh
                         .join("\n")
                 );
-                println!("{:?}", &lp);
-                println!("{:?}", self.input.1.clone());
+                //println!("{:?}", &lp);
+                //println!("{:?}", self.input.1.clone());
 
                 let mut ctl = clingo::control(self.input.1.clone())?;
                 ctl.add("base", &[], &lp)?;
